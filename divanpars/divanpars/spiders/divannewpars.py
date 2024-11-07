04adc46a33ad88520c9c311f2dc3a8bed731b08b
24-python <24_python@mail.ru>
2024-11-07 10:14:31 +0300

    Готовый спайдер для диванов и кресел

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -4,7 +4,14 @@ import scrapy
 class DivannewparsSpider(scrapy.Spider):
     name = "divannewpars"
     allowed_domains = ["https://divan.ru"]
-    start_urls = ["https://www.divan.ru/category/divany-i-kresla"]
+    start_urls = ["https://www.divan.ru/krasnoyarsk/category/divany-i-kresla"]
 
     def parse(self, response):
-        pass
+        divans = response.css('div.WdR1o')
+
+        for divan in divans:
+            yield {
+                'name': divan.css('div.lsooF span::text').get(),
+                'price': divan.css('div.pY3d2 span::text').get(),
+                'url': divan.css('a::attr(href)').get()
+            }
