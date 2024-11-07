932119dbf6ab2c68e4752ac68694d0d42b40119b
24-python <24_python@mail.ru>
2024-11-07 11:02:48 +0300

    Готовый парсер источников освещения диван.ру
    Сайт леруамерлен (леманапро) просит авторизации и выдает ошибку 401, поэтому делалось на диван.ру

--- svetpars/svetpars/spiders/svetnewpars.py
+++ svetpars/svetpars/spiders/svetnewpars.py
@@ -3,8 +3,15 @@ import scrapy
 
 class SvetnewparsSpider(scrapy.Spider):
     name = "svetnewpars"
-    allowed_domains = ["krasnoyarsk.lemanapro.ru"]
-    start_urls = ["https://krasnoyarsk.lemanapro.ru/catalogue/lyustry/"]
+    allowed_domains = ["https://www.divan.ru/krasnoyarsk"]
+    start_urls = ["https://www.divan.ru/krasnoyarsk/category/svet"]
 
     def parse(self, response):
-        pass
+        svets = response.css('div.WdR1o')
+
+        for svet in svets:
+            yield {
+                'name': svet.css('div.lsooF span::text').get(),
+                'price': svet.css('div.pY3d2 span::text').get(),
+                'url': svet.css('a::attr(href)').get()
+            }
